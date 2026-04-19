d6b59375e084000a6cdc86fc4084e57948be34fa
Shashank Nalkande <108921394+ShashankNalkande1@users.noreply.github.com>
2026-04-20 02:06:15 +0530

    Time: 31 ms (68.27%) | Memory: 12.5 MB (90.15%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        d = {}
+
+        
+        for i in magazine:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i] += 1
+
+        for i in ransomNote:
+            if i in d and d[i] > 0:
+                d[i] -= 1
+            else:
+                return False
+
+        return True
